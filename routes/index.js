70bc0a7ea7d46e92a084aa88a32f698b794ac585
Diego Balbis <dbalbis8@gmail.com>
2022-06-22 22:18:29 -0300

    Listo desafio con PUG

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const router = Router();
+/* Importando Clases y Productos */
+const ContenedorProductos = require('../Controllers/contenedor');
+const data = require('../Controllers/contenedor');
+
+/* Nueva Clase */
+const Productos = new ContenedorProductos(data);
+
+/* Mostrar todos los productos */
+
+router.get('/productos', async (req, res) => {
+  const respuesta = await Productos.getAll();
+  res.render('data.pug', { respuesta });
+});
+
+/* Agregar un producto */
+router.post('/productos', async (req, res) => {
+  const { title, price, thumbnail } = req.body;
+  if (isNaN(Number(price))) {
+    res.status(400).json({ message: 'Precio debe ser un numero!' });
+  } else {
+    await Productos.addProduct(title, price, thumbnail);
+    res.status(201).redirect('/productos');
+  }
+});
+
+module.exports = router;
